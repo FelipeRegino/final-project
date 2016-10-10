2d138b4758473d5e42d6d3d276a9a46cd239f20a
Pedro Guimaraes <pedroguimaraes@infojr.com.br>
2016-10-09 23:07:54 -0300

    Adiciona protótipo das funções e struct de leitos

---
+++ leitos.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "leitos.h"
+
+//implementação das funções
+
+void internaPaciente() {
+
+}
+
+void liberaPaciente() {
+
+}
+
+void imprimeRelatorio() {
+
+}
+
+void exibirMenu() {
+	int menu;
+	
+	printf("\t UNIDADE DE TRATAMENTO SEMI-INTENSIVO\n\n");
+	printf("1. Internar paciente.\n");
+	printf("2. Liberar paciente.\n");
+	printf("3. Imprimir relatório.\n");
+	printf("4. Sair.\n\n");
+	printf("Selecione a opção desejada.\n");
+
+}
+
+//main program
+int main() {
+
+menu = exibirMenu();
+
+	switch (menu) {
+		case 1:
+			internaPaciente();
+		break;
+		case 2:
+			liberaPaciente();
+		break;
+		case 3:
+			imprimeRelatorio();
+		break;
+		case 4:
+			printf("Você optou por sair do programa.\n");
+		break;
+		default:
+			printf("A operação não pode ser realizada.\n");
+		break;
+	}
+
+system("pause");
+return 0;	
+}
---
+++ leitos.h
@@ -0,0 +1,12 @@
+struct leitos {
+	char paciente[30];
+	char medico[30];
+	char code[5];
+	int status;
+};
+
+
+//protótipo das funções
+void internaPaciente();
+void liberaPaciente();
+void imprimeRelatorio();
