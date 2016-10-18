9d23a7613cb392d206e622c3d9907f4969065ac7
Felipe Regino <feliperegino@infojr.com.br>
2016-10-17 23:08:18 -0200

    Adicona algumas funções de farmacia e medicamentos

--- farm.c
+++ farm.c
@@ -1,20 +1,81 @@
 #include <stdio.h>
+#define MAX 1000
 
-struct famarcia {
-	char codigo[2];
-	char local[2];
-	char* medicamentos;
+struct farmacia {
+	char codigo;
+	char local;
+	struct farmacia *prim; 
+	struct farmacia *prox;
+	char* ListaMedicamentos;
 };
+typedef struct farmacia Farmacia;
 
-struct medicamentos {
-	char codigo[10];
+
+struct medicamento {
+	char codigo;
 	int qtd;
 	float id;
-	char nome[30];
-	char tipo[1];
-	char lote[4];
-	char validade[8];
+	char nome;
+	char tipo;
+	char lote;
+	char validade;
 };
+
+typedef struct medicamento Medicamento;
+typedef int Posicao;
+
+struct listaMedicamentos{
+	Medicamento medicamento[MAX];
+	Posicao Primeiro, Ultimo;
+};
+
+typedef struct listaMedicamentos ListaMedicamentos;
+
+Farmacia* cria_farm_central(char c, char l){
+	Farmacia *f=(Farmacia *) malloc(sizeof(Farmacia));
+	f->codigo = c;
+	f->local = l;
+	f->ListaMedicamentos = NULL;
+	f->prim = NULL;
+	f->prox = NULL;
+	return f;	
+}
+
+void insere_farm_satelite(Farmacia* fc, Farmacia* fs){
+	fs->prox = fc->prim;
+	fc->prim = fs;
+}
+
+ListaMedicamentos* incializaListaM(){
+	ListaMedicamentos* lista = (ListaMedicamentos*)malloc(sizeof(ListaMedicamentos));
+	return lista;
+}
+
+void limpa_lista(ListaMedicamentos* Lista){
+	Lista->Primeiro = 0;
+	Lista->Ultimo = Lista->Primeiro;	
+}
+
+int Vazia(ListaMedicamentos* Lista){
+	return(Lista->Primeiro == Lista->Ultimo);
+}
+
+void insere_medicamento(Medicamento* m, ListaMedicamentos* Lista, char c, int q, float i, char n, char t, char l, char v){
+	if(Lista->Ultimo>=MAX){
+		printf("Lista cheia\n");
+	}
+	else{
+		m->codigo = c;
+		m->qtd = q;
+		m->id = i;
+		m->nome = n;
+		m->tipo = t;
+		m->lote = l;
+		m->validade = v;
+		Lista->medicamento[Lista->Ultimo] = *m;
+		Lista->Ultimo++;
+	}
+}
 
 int main(){
 	return 0;
