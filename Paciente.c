34ff63766c6519c106bedeab6c971316ebd4323f
P4rdal <marcelodeiro@hotmail.com>
2016-10-27 11:34:16 -0300

    Pacitente
    Código atualizado e separado em c e h

--- Paciente.c
+++ Paciente.c
@@ -1,29 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
+#include <conio.h>
+#include "paciente.h"
 
-/* ESTRUTURA DE DADOS DO PACIENTE FALTA OS NOMES DOS PONTEIROS DAS LISTA DE : CONSULTA,PROCEDIMENTOS,MEDICAMENTOS E  */
-
-typedef struct Paciente{
-
-	char nome [30];
-	char codigo[10] ;
-	/* Lconsulta  "nome do ponteiro da lista de consulta" */
-	/* Lprocedimento "nome do ponteiro da lista de procedimentos" */
-	/* Lmedicamento "nome do ponteiro da lista de medicamentos" */
-	/* Ldiaria "nome do ponteiro da lista de medicamentos" */
-
-};
-typedef struct Paciente Tipopaciente;
-
-
-typedef struct listapaciente {
-
-   Tipopaciente  paciente;
-   Tipopaciente *proximo;
-
-};
-typedef struct listapaciente Lpaciente;
 
 
 Tipopaciente *criapaciente (){
@@ -43,6 +24,9 @@ return novo;
 
 }
 
+
+
+
 int VerificaListaVaziaPaciente (Lpaciente *listadepaciente){
 
 if (listadepaciente->proximo == NULL){
@@ -56,17 +40,32 @@ if (listadepaciente->proximo == NULL){
 
 Tipopaciente *CadastrarCliente (Lpaciente * listapaciente){
 
+system("cls");
+
+printf(" ================ TELA DE CADASTRO DE PACIENTES ================ \n \n");
+
+char temp;
+
 Lpaciente *aux;
 aux = crialistapaciente();
-
-printf("   digite o nome do paciente  ");
-scanf("%s",aux->paciente.nome);
-printf("   digite o nome codigo do paciente  ");
+printf("   Qual tipo de atendimento  1-Pronto A., 2-Ambulatorial, 3-internamento  ");
+scanf("%s",aux->paciente.Tipoatendimento);
+printf("   Digite o codigo do paciente ");
 scanf("%s",aux->paciente.codigo);
-
+printf("   Digite o nome do paciente  ");
+scanf("%s",aux->paciente.nome);
+printf("   Digite a Enfermidade do paciente  ");
+scanf("%s",aux->paciente.Enfermidade);
+printf("   Qual tipo de acomodacao 1- Leito Pronto A, 2-Enfermaria, 3-Apartamento, 4-Semi-intensiva, 5-Intensiva ");
+scanf("%s",aux->paciente.Tipodeacomodacao);
+printf("   Qual o plano de saude  ");
+scanf("%s",aux->paciente.CodigodoPlano);
 Lpaciente *oldhead = listapaciente->proximo;
 listapaciente->proximo = aux;
 aux->proximo = oldhead;
+printf("\n Paciente cadastrado com sucesso! \n");
+getch();
+system("cls");
 
 }
 
@@ -100,21 +99,13 @@ if (VerificaListaVaziaPaciente(listadepaciente) == 1){
 
 /* Essa função retorna o ponteiro de um paciente na lista de pacientes onde podem ser coletadas qualquer informação do paciente
    para usar essa função crie um variável Lpaciente *NomeDaVariavelTipoPonteiro = SelecionaPaciente;
-
    Ex:
-
   ==================================================================================
-
    Tipopaciente *DadosDoPaciente = SelecionaPaciente (Tipopaciente * listadepacientes) ;
-
    Quarto->Paciente  = DadosDoPaciente;
-
    ==================================================================================
-
    Para imprimir os dados do Paciente na Tela:
-
    ImprimeDadosPaciente (DadosDoPaciente);
-
    ==================================================================================
 */
 
@@ -144,7 +135,8 @@ ret = strcmp(cod, listapaciente->paciente.codigo);
 
  if ( ret!=0 && listapaciente->proximo == NULL ){
 
-    printf("CÓDIGO NÃO ENCONTRADO");
+    printf("CODIGO NAO ENCONTRADO");
+    return NULL;
 
  }
 
@@ -162,12 +154,26 @@ return PacienteSelecionado;
 
  void PesquisaPaciente (Lpaciente * listapaciente) //Retorna o Ponteiro do TipoCliente em uma lista de LPaciente
 {
-
+system("cls");
+printf(" ================ TELA DE PESQUISA DE PACIENTES ================ \n \n");
 Tipopaciente *pac;
 pac = criapaciente();
 pac = SelecionaPaciente(listapaciente);
+
+if ( pac == NULL){
+
+
+
+}else {
+
 printf(" NOME : %s \n",&pac->nome);
 printf(" CODIGO : %s \n",&pac->codigo);
+getch();
+
+
+}
+
+
 
 }
 
@@ -263,82 +269,5 @@ scanf("%s",listapaciente->paciente.nome);
 }
 
 }
-
-}
-
-
-Tipopaciente *retornapaciente(){}
-
-
-
-int menu(void)
-{
-	int opt;
-
-	printf("Escolha a opcao\n");
-	printf("1. Cadastrar paciente \n");
-	printf("2. Imprimir Todos Paciente\n");
-    printf("3. Pesquisar Paciente\n");
-    printf("4. Remover Paciete\n");
-    printf("5. Editar Paciete\n");
-	printf("Opcao: "); scanf("%d", &opt);
-
-	return opt;
-}
-
-
-
-void opcao(Lpaciente *lista, int op)
-{
-
-
-	switch(op){
-		case 1:
-			CadastrarCliente(lista);
-			break;
-		case 2:
-			ImprimeListaPacientes(lista);
-			break;
-        case 3:
-			PesquisaPaciente(lista);
-			break;
-        case 4:
-			RemovePaciente (lista);
-			break;
-		 case 5:
-			AlteraCadastroCliente(lista);
-			break;
-
-
-		default:
-			printf("Comando invalido\n\n");
-	}
-}
-
-
-
-
-
-main (){
-
-
-    int i = 0;
-
- Lpaciente *listadepacientes;
- listadepacientes = crialistapaciente();
-
-
-
-	int opt;
-
-	do{
-		opt=menu();
-		opcao( listadepacientes,opt);
-	}while(opt);
-
-	free( listadepacientes);
-	return 0;
-
-
 
 }
